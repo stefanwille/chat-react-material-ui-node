3724506dd6074f5755ccd2d4c95288897cd63c50
Stefan Wille <github@stefanwille.com>
2018-05-24 11:14:44 +0200

    Add deployment

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,5 +1,5 @@
 PROJECT_PATH=/home/www-data/sites/chatdemo.stefanwille.com/server/
-rsync -avz --delete --exclude '.git' --exclude 'node_modules' -e ssh --progress $PWD/ sw:$PROJECT_PATH
+rsync -avz --delete --exclude '.git' --exclude 'node_modules' -e ssh --progress $PWD/ www-data@88.99.14.3:$PROJECT_PATH
 ssh sw "cd $PROJECT_PATH && yarn"
 ssh sw "cd $PROJECT_PATH && yarn stop-production"
 ssh sw "cd $PROJECT_PATH && yarn start-production"
