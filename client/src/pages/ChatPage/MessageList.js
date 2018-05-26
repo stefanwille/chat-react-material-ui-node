0b7e16d70f74b4a8ebfbe56a9eb3fff4cf320372
Stefan Wille <github@stefanwille.com>
2018-05-26 11:34:25 +0200

    withStyles

--- client/src/pages/ChatPage/MessageList.js
+++ client/src/pages/ChatPage/MessageList.js
@@ -3,30 +3,34 @@ import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Avatar from "@material-ui/core/Avatar";
+import { withStyles } from "@material-ui/core/styles";
+
 import { format } from "date-fns";
 
-const Message = ({ message }) => {
+const stylesForMessage = {
+  avatar: { maxWidth: 32, maxHeight: 32, marginRight: 5 },
+  userName: {
+    display: "inline-block",
+    marginRight: 5,
+  },
+  timestamp: { display: "inline-block" },
+};
+
+const Message = withStyles(stylesForMessage)(({ message, classes }) => {
   const {
     user: { avatarUrl, userName },
     timestamp,
     text,
   } = message;
   return (
-    <Grid container style={{ justifyContent: "flex-start" }}>
-      <Avatar
-        style={{ maxWidth: 32, maxHeight: 32, marginRight: 5 }}
-        alt={userName}
-        src={avatarUrl}
-      />
+    <Grid container justify="flex-start">
+      <Avatar className={classes.avatar} alt={userName} src={avatarUrl} />
 
       <div>
-        <Typography
-          variant="body2"
-          style={{ display: "inline-block", marginRight: 5 }}
-        >
+        <Typography variant="body2" className={classes.userName}>
           {userName}
         </Typography>
-        <Typography variant="caption" style={{ display: "inline-block" }}>
+        <Typography variant="caption" className={classes.timestamp}>
           {format(timestamp, "HH:mm")}
         </Typography>
         <Typography variant="body1" paragraph>
@@ -35,12 +39,14 @@ const Message = ({ message }) => {
       </div>
     </Grid>
   );
-};
+});
 
-const MessageList = ({ messages }) => (
+const stylesForMessageList = {};
+
+const MessageList = ({ messages, classes }) => (
   <div>
     {messages.map(message => <Message key={message.id} message={message} />)}
   </div>
 );
 
-export default MessageList;
+export default withStyles(stylesForMessageList)(MessageList);
